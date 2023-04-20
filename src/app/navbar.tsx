d177190fd76b98853d20a407b6db1c2e87d9f7cb
Konrad <gladysz.konrad@gmail.com>
2023-04-20 12:39:08 +0100

    fix typo in navbar

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -6,7 +6,7 @@ export function Navbar() {
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <a href="/" className="flex items-center">
                     <ActivitiIcon className="h-8 mr-3" width="64" height="32" />
-                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Activiti Tookit</span>
+                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Activiti Toolkit</span>
                 </a>
                 {/* <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
                     <span className="sr-only">Open main menu</span>
