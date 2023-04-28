870ad1e66292a0f8a97c7144835ed18d5afdf602
Konrad <gladysz.konrad@gmail.com>
2023-04-28 09:20:53 +0100

    change default text and embed right arrow svg

--- src/app/fhir/page.tsx
+++ src/app/fhir/page.tsx
@@ -9,13 +9,15 @@ export default function Page() {
                     <h2 className={`mb-3 text-2xl font-semibold`}>
                         FHIR Resource Analyser{' '}
                         <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                          ...
+                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-arrow-right-short" viewBox="0 0 16 8">
+                                <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z" />
+                            </svg>
                         </span>
                     </h2>
                     <p
                         className={`m-0 max-w-[30ch] text-sm opacity-50`}
                     >
-                        Find in-depth information about Next.js features and API.
+                        Tool for viewing FHIR Resources.
                     </p>
                 </a>
             </div>
