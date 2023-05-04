f05c540aab00c2740a053fef68d14063591af38d
Konrad <gladysz.konrad@gmail.com>
2023-05-04 07:34:42 +0100

    move all svg to a single file

--- src/app/icons.tsx
+++ src/app/icons.tsx
@@ -19,3 +19,22 @@ export function ActivitiIcon(
         </svg>
     )
 }
+
+export function ArrowRightIcon({
+    className,
+    alt,
+    width,
+    height
+}: {
+    className?: string
+    alt: string
+    width: string
+    height: string
+}) {
+    return (
+        <svg className={`${className}`} xmlns="http://www.w3.org/2000/svg" width={width} height={height} fill="currentColor" viewBox="0 0 16 8">
+            <title>{alt}</title>
+            <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z" />
+        </svg>
+    )
+}
--- src/app/json/page.tsx
+++ src/app/json/page.tsx
@@ -1,50 +1,49 @@
+import { ArrowRightIcon } from "../icons"
+
 export default function Page() {
     return (
         <main className="flex flex-col items-center justify-between p-24">
             <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-2 lg:text-left">
-                <a
-                    href="https://jsonpathfinder.com/"
-                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                    rel="noopener noreferrer"
-                    target="_blank"
-                >
-                    <h2 className={`mb-3 text-2xl font-semibold`}>
-                        JSON Path Finder{' '}
-                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-arrow-right-short" viewBox="0 0 16 8">
-                                <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z" />
-                            </svg>
-                        </span>
-                    </h2>
-                    <p
-                        className={`m-0 max-w-[30ch] text-sm opacity-50`}
-                    >
-                        Web app to find the path of an item in a JSON object. Developed by Joe Beach.
-                    </p>
-                </a>
-                <a
+                <CardExternal href="https://jsonpathfinder.com/" title="JSON Path Finder" text="Web app to find the path of an item in a JSON object. Developed by Joe Beach." />
+                <CardExternal
                     href="https://www.json.org/json-en.html"
-                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                    rel="noopener noreferrer"
-                    target="_blank"
-                >
-                    <h2 className={`mb-3 text-2xl font-semibold`}>
-                        JSON Specification{' '}
-                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-arrow-right-short" viewBox="0 0 16 8">
-                                <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z" />
-                            </svg>
-                        </span>
-                    </h2>
-                    <p
-                        className={`m-0 max-w-[30ch] text-sm opacity-50`}
-                    >
-                        (JavaScript Object Notation) is a lightweight data-interchange
-                        format. It is easy for humans to read and write. It is easy for machines to
-                        parse and generate.
-                    </p>
-                </a>
+                    title="JSON Specification"
+                    text="(JavaScript Object Notation) is a lightweight data-interchange
+                          format. It is easy for humans to read and write. It is easy for machines to
+                          parse and generate."
+                />
             </div>
         </main>
+    )
+}
+
+function CardExternal({
+    href,
+    title,
+    text
+}: {
+    href: string
+    title: string
+    text: string
+}) {
+    return (
+        <a
+            href={href}
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            rel="noopener noreferrer"
+            target="_blank"
+        >
+            <h2 className={`mb-3 text-2xl font-semibold`}>
+                {title}{' '}
+                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                    <ArrowRightIcon width="32" height="32" alt="Arrow Right" />
+                </span>
+            </h2>
+            <p
+                className={`m-0 max-w-[30ch] text-sm opacity-50`}
+            >
+                {text}
+            </p>
+        </a>
     )
 }
