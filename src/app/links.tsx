fa6374eca7f8909c01c38cb9899a57dc4a5416a9
Konrad <gladysz.konrad@gmail.com>
2023-05-05 14:56:24 +0100

    add license

--- src/app/links.tsx
+++ src/app/links.tsx
@@ -3,11 +3,29 @@ import Link from "next/link"
 export function FhirTypeDef({
     type,
     className
-} : {
+}: {
     type: string,
     className?: string
 }) {
     return (
         <Link className={`${className}`} href={`https://www.hl7.org/fhir/${type}.html`} target="_blank" rel="noopener noreferrer"><span className="underline">{`https://www.hl7.org/fhir/${type}.html`}</span></Link>
+    )
+}
+
+export function LinkExternal(
+    {
+        className,
+        href,
+        text
+    }: {
+        className?: string
+        href: string
+        text: string
+    }
+) {
+    return (
+        <Link className={className} href={href} target="_blank" rel="noopener noreferrer">
+            {text}
+        </Link>
     )
 }
